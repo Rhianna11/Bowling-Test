d82a35a3162237e597be39d5509c51315deeedbd
Rhianna11 <rhiannathwaite@gmail.com>
2020-08-10 18:07:34 +0100

    Add project files.

---
+++ BowlingTest/Models/Session.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingTest.Models
+{
+    class Session
+    {
+        [JsonProperty("gameId")]
+        public string GameId { get; set; }
+        [JsonProperty("throws")]
+        public List<string> Throws { get; set; }
+    }
+}
---
+++ BowlingTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using BowlingTest.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BowlingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Documents\BowlingTest\Bin\Games.json";
+
+            string jsonString = File.ReadAllText(path);
+
+            List<Session> games = JsonConvert.DeserializeObject<List<Session>>(jsonString);
+
+
+        }
+    }
+}
