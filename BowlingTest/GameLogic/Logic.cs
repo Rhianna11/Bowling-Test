68548854338783546f32a9c9be363bb1c33ca086
Rhianna11 <rhiannathwaite@gmail.com>
2020-08-13 20:55:32 +0100

    Added 10th frame logic and out of range  index checks

--- BowlingTest/GameLogic/Logic.cs
+++ BowlingTest/GameLogic/Logic.cs
@@ -6,6 +6,7 @@ namespace BowlingTest.GameLogic
 { 
     public class Logic
     {
+        //check current and consecutive throw to see if it's a Double
         public static bool IsDouble(string currThrow, string throw1)
         {
             if (currThrow == "X" && throw1 == "X")
@@ -16,9 +17,43 @@ namespace BowlingTest.GameLogic
             return false;
         }
 
+        //check current and consecutive 2 throws to see if it's a Turkey
         public static bool IsTurkey(string currThrow, string throw1, string throw2)
         {
             if (currThrow == "X" && throw1 == "X" && throw2 == "X")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        //If current throw is strike then check second throw of next frame for spare
+        public static bool isStrikeThenSpare(string throw2)
+        {
+            if (throw2 == "/")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsOutOfIndexForStrike(int numOfThrows, int listLength)
+        {
+            //Minus length of list from num of throws to see how many indexes are left for strike
+            if (listLength - numOfThrows <= 2)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsOutOfIndexForSpare(int numOfThrows, int listLength)
+        {
+            //Minus length of list from num of throws to see how many indexes are left for psare
+            if (listLength - numOfThrows <= 1)
             {
                 return true;
             }
--- BowlingTest/Program.cs
+++ BowlingTest/Program.cs
@@ -18,6 +18,7 @@ namespace BowlingTest
 
                 jsonString = File.ReadAllText(path);
 
+                //Replace miss and foul chars with a zero score
                 jsonString = jsonString.Replace("-", "0");
                 jsonString = jsonString.Replace("F", "0");
             }
@@ -26,69 +27,118 @@ namespace BowlingTest
                 Console.WriteLine(e.Message);
             }
 
+            //Deserialize JSON
             List<Session> sessions = JsonConvert.DeserializeObject<List<Session>>(jsonString);
-
+        
             foreach (var session in sessions)
             {
                 Console.WriteLine(session.GameId);
 
-                int score = 0, throwCount = 1, frameCount = 1; 
-                bool isTurkey = false, isDouble = false;
+                int score = 0, throwCount = 0, frameCount = 1;
+                bool isTurkey = false, isDouble = false, isStrikeThenSpare = false, isOutOfIndexForStrike = false, isOutOfIndexForSpare = false, isTenthFrame = false;
 
+                //For loop for counting each throw
                 for (var i = 0;  i < session.Throws.Count; i++)
                 {
+                    throwCount++;
+                    //Set bool to true when framecount is ten
+                    if (frameCount == 10)
+                    {
+                        isTenthFrame = true;
+                    }
+                    // Switch case for session throws
+                    switch (session.Throws[i])
+                    { 
+                        case "X":                                                   
+                            //Check if next two indexes for current Strike can be accessed
+                            isOutOfIndexForStrike = GameLogic.Logic.IsOutOfIndexForStrike(i, session.Throws.Count);
+
+                            //If index is not out of range
+                            if (!isOutOfIndexForStrike)
+                            {                               
+                                    score += 10;
+
+                                    //Pass current throw and next two throws to method
+                                    isTurkey = GameLogic.Logic.IsTurkey(
+                                        session.Throws[i],
+                                        session.Throws[i + 1],
+                                        session.Throws[i + 2]);
+                                    //Pass second throw to method
+                                    isStrikeThenSpare = GameLogic.Logic.isStrikeThenSpare
+                                        (session.Throws[i + 2]);
+
+                                    //Check to see if Turkey (three consecutive Strikes)
+                                    if (!isTurkey)
+                                    {
+                                        isDouble = GameLogic.Logic.IsDouble(
+                                        session.Throws[i],
+                                        session.Throws[i + 1]);
+                                    }
+                                    //Check if current throw and next throw are Strikes
+                                    if (isDouble)
+                                    {
+                                        score += 10 + Convert.ToInt32(session.Throws[i + 2]);
+                                    }
+                                    //If Turkey is true then add 20 to score
+                                    else if (isTurkey)
+                                    {
+                                        score += 20;
+                                    }
+                                    //If there is a Spare after the current strike
+                                    else if (isStrikeThenSpare)
+                                    {
+                                        score += 10;
+                                    }
+                                    //If non of the bools are satisfied then add the next two throws as normal
+                                    else
+                                    {
+                                        score += Convert.ToInt32(session.Throws[i + 1]) + Convert.ToInt32(session.Throws[i + 2]);
+                                    }
+                            }
+
+                            break;
+
+                            
+                        case "/":
+                            //Check if next two indexes for current Spare can be accessed
+                            isOutOfIndexForSpare = GameLogic.Logic.IsOutOfIndexForSpare(i, session.Throws.Count);
+
+                            //If index is not out of range
+                            if (!isOutOfIndexForSpare)
+                            {
+                                score += 10 - Convert.ToInt32(session.Throws[i - 1]) + Convert.ToInt32(session.Throws[i + 1]);
+                            }
+                            break;
+
+                            //If normal throw (digits)
+                        default:
+                            if (i > 2 && isTenthFrame)
+                            {
+                                //Check to see if last frame was not a Strike to avoid overcounting score
+                                if (session.Throws[i - 2] != "X")
+                                {
+                                    score += Convert.ToInt32(session.Throws[i]);
+                                }
+                            }
+                            else
+                            {
+                                score += Convert.ToInt32(session.Throws[i]);
+                            }
+                            break;
+
+                    }
+                    //If current throw is Strike, increment framecount
+                    if (session.Throws[i] == "X")
+                    {
+                        throwCount = 0;
+                        frameCount++;
+                    }
+                    //If throwcount is 2 then set to zero and incrememnt framecount
                     if (throwCount == 2)
                     {
-                        throwCount = 1;
+                        throwCount = 0;
                         frameCount += 1;
-                    }
-
-                    switch (session.Throws[i])
-                    {
-                        case "X":
-                            score += 10;
-
-                                isTurkey = GameLogic.Logic.IsTurkey(
-                                    session.Throws[i],
-                                    session.Throws[i + 1],
-                                    session.Throws[i + 2]);
-
-                                if (!isTurkey)
-                                {
-                                    isDouble = GameLogic.Logic.IsDouble(
-                                    session.Throws[i],
-                                    session.Throws[i + 1]);
-                                }
-
-                                if (isDouble)
-                                {
-                                    score += 10 + Convert.ToInt32(session.Throws[i + 2]);
-                                }
-
-                                else if (isTurkey)
-                                {
-                                    score += 20;
-                                }
-
-                                else if (session.Throws[i + 2] == "/")
-                                {
-                                    score += 10;
-                                }
-                                else
-                                {
-                                    score += Convert.ToInt32(session.Throws[i + 1]) + Convert.ToInt32(session.Throws[i + 2]);
-                                }
-
-                            break;
-
-                        case "/":
-                            score += 10 - Convert.ToInt32(session.Throws[i - 1]) + Convert.ToInt32(session.Throws[i + 1]);
-                            break;
-
-                        default:
-                            score += Convert.ToInt32(session.Throws[i]);                           
-                            break;
-                    }
+                    }                   
                 }
 
                 Console.WriteLine(score);
